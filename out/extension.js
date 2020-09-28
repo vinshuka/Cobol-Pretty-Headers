8d861ea6d6f59b496b8faf31920b3ccd04565d61
Vinshuka <melissa.ann.christie@gmail.com>
2020-09-28 09:59:44 -0400

    Fixed bug with spliting headers, removed unecessary conditional for empty string tokens, added extension configurations

--- out/extension.js
+++ out/extension.js
@@ -6,15 +6,24 @@ function activate(context) {
     const disposable = vscode.commands.registerCommand('extension.generateHeaderCode', function () {
         // Get the active text editor
         const editor = vscode.window.activeTextEditor;
+        //pulls user specified level numbers from user settings
+        //need to add condition that fires off an error if the highest !> lowest
+        const highestLevel = vscode.workspace.getConfiguration().get('cobolprettyheaders.setHighestLevelNumber');
+        const lowestLevel = vscode.workspace.getConfiguration().get('cobolprettyheaders.setLowestLevelNumber');
+        const condensed = vscode.workspace.getConfiguration().get('cobolprettyheaders.condensedHeaders');
+        const outputLocation = vscode.workspace.getConfiguration().get('cobolprettyheaders.setPrintOutputField');
         if (editor) {
             // start off by getting the document in the current editor and all the text in the document
             const document = editor.document;
             const allText = document.getText();
             // Split the text into individual lines
-            const allLines = allText.split("\n");
-            console.log(allLines);
+            const allLines = allText.split("\r\n");
+            //editor.edit is the main function that edits the current document
             editor.edit(editBuilder => {
+                //we define Postion for our calls to insert, this sets the starting line to the number of lines the header is
                 const pos = new vscode.Position(allLines.length, 0);
+                //title pulls the first part of the line in the header that is not spaces
+                //we may include a setting to allow the user to define these either as a text input box or as a configuration setting
                 function getTitle(text) {
                     for (let i = 0; i < text.length; i++) {
                         if (text[i].trim().length != 0) {
@@ -22,21 +31,29 @@ function activate(context) {
                         }
                     }
                 }
-                // create our own type called headerRecord and give it the title and data, then we can iterate a list of them to write them to 
-                //the text document
+                //process line creates a HeaderRecord object for each line, defining the title and data fields
                 function processLine(line) {
                     const parts = line.split(/(\s+)/);
+                    console.log(parts);
                     const title = getTitle(parts) + '-HDR';
                     const data = [];
-                    parts.forEach(part => {
-                        if (part.length != 0) {
-                            data.push('\n\t05 FILLER	PIC X(' + part.length + ')\tVALUE '
+                    //if the used has set condensed headers to true, the data field will contain the entire line of the header
+                    if (condensed) {
+                        data.push('\n\t' + lowestLevel + ' FILLER\tPIC X(' + line.length + ')\tVALUE "' + line + '"');
+                    }
+                    else {
+                        parts.forEach(part => {
+                            //each data field starts on a new line and is tabbed 1 level in, if the part of the header
+                            //being defined only contains spaces, a ternary condition sets the value to SPACES
+                            data.push('\n\t' + lowestLevel + ' FILLER	PIC X(' + part.length + ')\tVALUE '
                                 + (!part.trim().length ? "SPACES" : '"' + part + '"'));
-                        }
-                    });
+                        });
+                    }
+                    //define and return a completed header record object
                     const h = { title: title, data: data };
                     return h;
                 }
+                //process header returns an array containing HeaderRecords for each line of the header
                 function processHeader(lines) {
                     const headerData = [];
                     lines.forEach(line => {
@@ -45,35 +62,46 @@ function activate(context) {
                     });
                     return headerData;
                 }
+                //generate data fields does the actual writting of the header datafields to our document
                 function generateDataFields(headerData) {
                     headerData.forEach(hData => {
-                        editBuilder.insert(pos, hData.title + ".");
+                        editBuilder.insert(pos, highestLevel + " " + hData.title + ".");
                         for (let i = 0; i < hData.data.length; i++) {
                             editBuilder.insert(pos, hData.data[i]);
                         }
+                        //this puts two lines between the data fields and the write procedure
                         editBuilder.insert(pos, '\n\n');
                     });
                 }
-                function generateCode() {
-                    const header = processHeader(allLines);
-                    for (let i = 0; i < header.length; i++) {
-                        editBuilder.insert(pos, '\n');
-                    }
-                    generateDataFields(header);
-                    generateWriter(header);
-                }
+                //generates the write procedure for the header, currently assumes 1 line between each header line, we will add functionality
+                //to allow for more than 1 line between header lines, as defined by header text
                 function generateWriter(headerData) {
                     editBuilder.insert(pos, '\n');
                     editBuilder.insert(pos, '\nwrite-hdrs.');
                     for (let i = 0; i < headerData.length; i++) {
                         if (i === 0) {
-                            editBuilder.insert(pos, "\nWRITE 'output' FROM " + headerData[i].title + '\n\t AFTER ADVANCING PAGE');
+                            //first line of the header always has after advancing page, may include setting to change this, not sure
+                            editBuilder.insert(pos, "\nWRITE '" + outputLocation + "' FROM " + headerData[i].title + '\n\t AFTER ADVANCING PAGE');
                         }
                         else {
-                            editBuilder.insert(pos, "\nWRITE 'output' FROM " + headerData[i].title + '\n\t AFTER ADVANCING 1 LINE');
+                            editBuilder.insert(pos, "\nWRITE '" + outputLocation + "' FROM " + headerData[i].title + '\n\t AFTER ADVANCING 1 LINE');
                         }
                     }
                 }
+                //generate code, uses all of the above functions to generate the cobol code to write the header as written in the document
+                //by the user
+                function generateCode() {
+                    //creates the header data
+                    const header = processHeader(allLines);
+                    //moves down the cursor as to not over write the user submitted text
+                    for (let i = 0; i < header.length; i++) {
+                        editBuilder.insert(pos, '\n');
+                    }
+                    //write the code
+                    generateDataFields(header);
+                    generateWriter(header);
+                }
+                //activating Generate Headers calls this single function to do so
                 generateCode();
             });
         }
